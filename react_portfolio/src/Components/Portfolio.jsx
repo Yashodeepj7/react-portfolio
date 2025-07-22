9f6fa830bb0e14e6081ce1373dd40f0edff157d1
YashodeepWork <yashodeepwork7@gmail.com>
2025-07-22 23:56:57 +0530

    all updated final code

--- react_portfolio/src/Components/Portfolio.jsx
+++ react_portfolio/src/Components/Portfolio.jsx
@@ -59,7 +59,7 @@ const Portfolio = () => {
                   <Card className="text-white bg-dark border border-secondary w-100 shadow-lg" style={{ minHeight: "400px" }}>
                     <Card.Img
                       variant="top"
-                      src={`${import.meta.env.VITE_BACKEND_URL}images/${project.image}`} // Using default image since API doesn't provide one
+                      src={`${import.meta.env.VITE_BACKEND_URL}/images/${project.image}`} // Using default image since API doesn't provide one
                       alt={project.projectName}
                       style={{ height: "200px", objectFit: "cover" }}
                       className="hover-zoom hover-shadow"
